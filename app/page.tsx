041af14ffac8f106111448ef1a8245c8b1ec245d
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-12-01 14:12:25 -0300

    feat: updating to be pngs instead of svgs

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,32 @@
 
 export default function Home() {
   return (
-    <>
-      <div className="px-3 md:px-6 flex flex-wrap md:flex-nowrap md:justify-around my-10">
-        <div className="my-6 md:my-2 flex justify-center items-center w-full">
-          <img src="/esquerda.svg" alt="esquerda" />
+    <div className="flex flex-wrap md:flex-nowrap flex-col min-h-screen px-3 md:px-6">
+      {/* First Row */}
+      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 my-12">
+        <div className="flex justify-center items-center">
+          <img src="/esquerda.png" alt="esquerda" width={300} />
         </div>
-
-        <div className="my-6 md:my-2 flex justify-center items-center w-full">
-          <img src="/meio.svg" alt="meio" />
+        <div className="flex justify-center items-center">
+          <img src="/meio.png" alt="meio" width={800} />
         </div>
-
-        <div className="my-6 md:my-2 flex justify-center items-center w-full">
-          <img src="/direita.svg" alt="direita" />
-        </div>
-      </div>
-      <div className="px-3 md:px-6 flex flex-col md:flex-row flex-wrap md:flex-nowrap justify-around md:justify-around">
-        <div className=" my-6 md:my-2 flex flex-wrap gap-10 justify-center">
-          <img src="/baixo-esquerda.svg" alt="esquerda" />
-          <img src="/grv.svg" alt="esquerda" />
-          <img src="/baixo-direita.svg" alt="esquerda" />
+        <div className="flex justify-center items-center">
+          <img src="/direita.png" alt="direita" width={230} />
         </div>
       </div>
-    </>
+
+      {/* Second Row */}
+      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 my-12">
+        <div className="flex justify-center items-center">
+          <img src="/baixo-esquerda.png" alt="esquerda" width={300} />
+        </div>
+        <div className="flex justify-center items-center">
+          <img src="/grv.png" alt="meio" />
+        </div>
+        <div className="flex justify-center items-center">
+          <img src="/baixo-direita.png" alt="direita" width={300} />
+        </div>
+      </div>
+    </div>
   );
 }
