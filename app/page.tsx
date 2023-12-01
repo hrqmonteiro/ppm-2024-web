2b1e4ab2d8ac6f74a91ad5588effb26e74962d37
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-12-01 16:35:53 -0300

    feat: adding media query conditions on border and order

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,32 @@ export default function Home() {
   return (
     <div className="flex flex-wrap md:flex-nowrap flex-col min-h-screen px-3 md:px-6">
       {/* First Row */}
-      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 mt-12 border-b border-b-white">
+      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 mt-12 md:border-b md:border-b-white">
         <div className="flex justify-center items-center">
           <img src="/esquerda.png" alt="esquerda" width={300} />
         </div>
         <div className="flex justify-center items-center">
-          <img src="/meio.png" alt="meio" width={800} />
+          <img
+            className="hidden md:block"
+            src="/meio.png"
+            alt="meio"
+            width={800}
+          />
+          <img src="/direita.png" width={230} className="md:hidden" alt="" />
         </div>
-        <div className="flex justify-center items-center">
-          <img src="/direita.png" alt="direita" width={230} />
+        <div className="flex justify-center items-center border-b border-b-white md:border-b-0">
+          <img
+            className="hidden md:block"
+            src="/direita.png"
+            alt="direita"
+            width={230}
+          />
+          <img
+            className="md:hidden"
+            src="/meio.png"
+            alt="direita"
+            width={800}
+          />
         </div>
       </div>
 
