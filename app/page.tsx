cd8f2a6aca835793a3ebdcceaeb3c89a5de90375
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-12-01 16:31:41 -0300

    feat: removing unused grv logo and adding division line

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-wrap md:flex-nowrap flex-col min-h-screen px-3 md:px-6">
       {/* First Row */}
-      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 my-12">
+      <div className="flex flex-wrap md:flex-nowrap gap-10 md:gap-0 justify-around items-center min-h-1/2 mt-12 border-b border-b-white">
         <div className="flex justify-center items-center">
           <img src="/esquerda.png" alt="esquerda" width={300} />
         </div>
@@ -23,7 +23,7 @@ export default function Home() {
           <img src="/baixo-esquerda.png" alt="esquerda" width={300} />
         </div>
         <div className="flex justify-center items-center">
-          <img src="/grv.png" alt="meio" />
+          {/* <img src="/grv.png" alt="meio" /> */}
         </div>
         <div className="flex justify-center items-center">
           <img src="/baixo-direita.png" alt="direita" width={300} />
