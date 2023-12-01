a151da21e8f963f2ca67f48ed73d8a0d60208e1a
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-12-01 13:30:30 -0300

    feat: page on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,27 @@
 
 export default function Home() {
   return (
-    <main className="w-full flex min-h-screen flex-col">
-      <img src="/path1.svg" alt="PPM" width="100%" />
-      <img src="/path2.svg" alt="PPM" width="100%" />
-    </main>
+    <>
+      <div className="px-3 md:px-6 flex flex-wrap md:flex-nowrap md:justify-around my-10">
+        <div className="my-6 md:my-2 flex justify-center items-center w-full">
+          <img src="/esquerda.svg" alt="esquerda" />
+        </div>
+
+        <div className="my-6 md:my-2 flex justify-center items-center w-full">
+          <img src="/meio.svg" alt="meio" />
+        </div>
+
+        <div className="my-6 md:my-2 flex justify-center items-center w-full">
+          <img src="/direita.svg" alt="direita" />
+        </div>
+      </div>
+      <div className="px-3 md:px-6 flex flex-col md:flex-row flex-wrap md:flex-nowrap justify-around md:justify-around">
+        <div className=" my-6 md:my-2 flex flex-wrap gap-10 justify-center">
+          <img src="/baixo-esquerda.svg" alt="esquerda" />
+          <img src="/grv.svg" alt="esquerda" />
+          <img src="/baixo-direita.svg" alt="esquerda" />
+        </div>
+      </div>
+    </>
   );
 }
